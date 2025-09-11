26e3ec00ac8755c175751c2954456465a4d2ef4b
wuwunchik <atochin99@mail.ru>
2025-09-11 17:19:03 +0800

    fix: Исправлена отправка запросов через swagger

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"wuwunchik.github.io/api/database"
 	"wuwunchik.github.io/api/routes"
@@ -14,6 +15,11 @@ func main() {
 	router := mux.NewRouter()
 	routes.RegisterRoutes(router)
 
+	// Настройка CORS
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
+
 	log.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(headersOk, originsOk, methodsOk)(router)))
 }
